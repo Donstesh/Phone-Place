ff0682a9b651eae244e9859f5ec35a46fbbdf37e
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-30 09:49:30 +0300

    creacted a products page

---
+++ products.php
@@ -0,0 +1,13 @@
+<?php
+    
+?>
+
+<html>
+    <head>
+        <title>Phone Place</title>
+    </head>
+    <body>
+        <h1>Buy vitu</h1>
+        <p>One stop shop to all second hand phones</p>
+    </body>
+</html>
