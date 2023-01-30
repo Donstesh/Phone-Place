da83c98dc400dcd8602333c508a5bffc354dfbb5
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-30 10:48:33 +0300

    added navbar

--- navbar.php
+++ navbar.php
@@ -12,47 +12,51 @@
 
     <ul>
     <li><a class="active" href="#home">Home</a></li>
-    <li><a href="#">Products</a></li>
-    <li><a href="#">Sell</a></li>
-    <li><a href="#">Contact Seller</a></li>
-    <li><a href="#">Sign Up</a></li>
-    <li><a href="#">Login</a></li>
-    <li><a href="#">Logout</a></li>
+    <li><a href="products.php">Products</a></li>
+    <li><a href="sell.php">Sell</a></li>
+    <li><a href="contact-seller.php">Contact Seller</a></li>
+    <li><a href="register.php">Sign Up</a></li>
+    <li><a href="login.php">Login</a></li>
+    <li><a href="logout.php">Logout</a></li>
     </ul>
 
-    </body>
+  </body>
     <style>
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0px;
-  overflow: hidden;
-  background-color: lightgray;
-}
+      *{
+        margin: 0;
+        padding: 0;
+      }
+      ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0px;
+        overflow: hidden;
+        background-color: lightgray;
+      }
 
-li {
-  float: left;
-}
+      li {
+        float: left;
+      }
 
-li a {
-  display: block;
-  color: blue;
- font-size:20px;
-  text-align: center;
-  padding: 10px 20px;
-  text-decoration: none;
-  display:flex;
-  justify-content:right;
+      li a {
+        display: block;
+        color: blue;
+      font-size:20px;
+        text-align: center;
+        padding: 10px 20px;
+        text-decoration: none;
+        display:flex;
+        justify-content:right;
 
-}
-.active{
-background-color: gray;
-color: white;
-}
-li a:hover {
-  background-color: orange;
-  color: white;
-}
+      }
+      .active{
+      background-color: gray;
+      color: white;
+      }
+      li a:hover {
+        background-color: orange;
+        color: white;
+      }
 </style>
 </html>
 
