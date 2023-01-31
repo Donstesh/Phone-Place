3454614fd284c87fd68619528a0ab253f1e7cd7e
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-31 14:23:11 +0300

    created connection

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpassword = "";
-$dbname = "login_sample_db";
+$dbname = "phone_place";
 
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname))
