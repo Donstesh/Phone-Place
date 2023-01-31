e8711a8ddc03df6a8ab1c1f5b7ca8c16de7b5949
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-31 12:26:46 +0300

    modified registration page

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
    include("functions.php");
 
 
-if($_SERVER['REQUEST_METHOD'] == "POST"){
+   if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['phonenumber'])) {
     // SOMETHING WAS POSTED
    $username = $_POST['username'];
    $password = $_POST['password'];
