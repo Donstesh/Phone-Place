6c21ee70f3644a043e3310b4c2f65bfb1cb8e05d
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-02-02 11:24:25 +0300

    inserted  data to the database

--- connection.php
+++ connection.php
@@ -5,20 +5,24 @@ $pass_word = $_POST['pass_word'];
 $confirm_password = $_POST['confirm_password'];
 $phonenumber = $_POST['phonenumber'];
 
-
-
 // database connection
+$conn = new mysqli('localhost', 'root', '', '', 'phone_place');
 
-$conn = new mysqli('localhost', 'root', '', 'phone_place');
-
-if($conn-> connect_error){
-    die('connection failed :' . $conn->connect_error);
+if($conn->connect_error){
+    die('connection failed: ' . $conn->connect_error);
 } else {
-    $stmt = $conn->prepare("insert into registration(name, email, pass_word, confirm_password, phonenumber)
-    values(?,?,?,?,?)");
-    $stmt->bind_param("ssssi", $name, $email, $pass_word, $confirm_password, $phonenumber);
-    $stmt->execute();
-    echo "Registration successfully";
-    $stmt->close();
-    $conn->close();
+    // Check if password and confirm password match
+    if($pass_word != $confirm_password){
+        echo "Passwords do not match";
+    } else {
+        // Insert data into the database
+        $stmt = $conn->prepare("INSERT INTO registration(name, email, pass_word, confirm_password, phonenumber) VALUES (?,?,?,?,?)");
+        $stmt->bind_param("sssi", $name, $email, $pass_word, $confirm_password, $phonenumber);
+        $stmt->execute();
+        echo "Registration successful";
+        $stmt->close();
+        $conn->close();
+    }
 }
+
+?>
