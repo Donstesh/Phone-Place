829f3256e9c04d3a4787e20a322c8e20981a7127
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-02-03 15:17:35 +0300

    created index page

--- index.php
+++ index.php
@@ -8,11 +8,7 @@ include('navbar.php');
   	$_SESSION['msg'] = "You must log in first";
   	header('location: login.php');
   }
-  if (isset($_GET['logout'])) {
-  	session_destroy();
-  	unset($_SESSION['username']);
-  	header("location: login.php");
-  }
+  
 ?>
 <!DOCTYPE html>
 <html>
