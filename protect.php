0f3d6c5b757b6206930be1a3b11ec9b6fc784551
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-02-06 13:07:08 +0300

    created protect page

--- protect.php
+++ protect.php
@@ -1,12 +1,12 @@
-<?php
+<!-- <?php
 
-  session_start();
+  // session_start();
 
 //checking if the session exists
 //if the session is not set it redirects the user to the login form/page
 
-  if(! isset($_SESSION["logged_in"])){
-      header("location:login.php");
-  }
+  // if(! isset($_SESSION["logged_in"])){
+  //     header("location:login.php");
+  // }
 
-?>
+?> -->
