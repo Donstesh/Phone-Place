40f154d322abd1ccd901a7e0965ba21e9c16618b
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-02-06 12:16:56 +0300

    created sell bar

--- sell.php
+++ sell.php
@@ -3,4 +3,21 @@
 include 'protect.php';
     session_start();
 ?>
-<h1>sell page </h1>
+
+
+<form action="/add-listing" method="POST" enctype="multipart/form-data">
+    <h3>Upload Item</h3>
+    <label>Title:</label>
+    <input type="text" name="title">
+    <label>Description:</label>
+    <input type="text" name="description">
+    <label>Price:</label>
+    <input type="number" name="price">
+    <label>Phone Number:</label>
+    <input type="number" name="phonenumber">
+    <label>Location:</label>
+    <input type="text" name="location">
+    <label>Images:</label>
+    <input type="file" name="product-image" id="" multiple>
+    <input type="submit" class="btn btn-primary">
+</form>
